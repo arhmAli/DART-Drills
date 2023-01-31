91af1583d9fef491e7fbf0b96148c7affd9b64ac
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-01-31 08:42:23 +0500

    classfive assignment added

---
+++ flutterCrashCourseFiles/dartClass5.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+void main() {
+  var capacity = 70;
+  print("The capacity of the libray is $capacity");
+
+  var firstClass = BooksShelf();
+
+  var numGen = firstClass.booksAvail();
+
+  print("The number of books in first shelf are $numGen");
+
+  var secondClass = SecondBooksShelf();
+
+  var secNumGen = secondClass.booksAvail();
+  print("The number of books in second shelf are $secNumGen");
+  numGen + secNumGen <= capacity
+      ? print("The shelfs in library have space for more")
+      : print("The shelfs in library does not have space for more");
+}
+
+class BooksShelf {
+  booksAvail() {
+    Random random = new Random();
+    int randNum = random.nextInt(100);
+    return randNum;
+  }
+}
+
+class SecondBooksShelf {
+  booksAvail() {
+    Random random = new Random();
+    int randNum = random.nextInt(100);
+    return randNum;
+  }
+}
