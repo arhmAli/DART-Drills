22592fd4bff3a8ac4c03c56e6b3cb82d22797e9f
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-16 10:05:57 +0500

    solved evenOdd in dart

--- CodeWarProblems/oddEven.dart
+++ CodeWarProblems/oddEven.dart
@@ -1,6 +1,26 @@
+//Problem Statement
+
+Create a function that takes an integer
+as an argument and returns "Even" for even numbers
+or "Odd" for odd numbers.
+
 
 
 //Solution
 String evenOrOdd(int number) {
   return number%2==0?"Even":"Odd";
+}
+
+//Test Cases
+import "package:test/test.dart";
+import "package:solution/solution.dart";
+
+void main() {
+  test('Basic tests', () {
+    expect(evenOrOdd(2), equals('Even'));
+    expect(evenOrOdd(1), equals('Odd'));
+    expect(evenOrOdd(0), equals('Even'));
+    expect(evenOrOdd(7), equals('Odd'));
+    expect(evenOrOdd(-1), equals('Odd'));
+  });
 }
