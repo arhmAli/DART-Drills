02c04395213b90ac8a7385049aadcee24c30d215
Arham Khan <110810048+arhmAli@users.noreply.github.com>
2023-02-26 01:08:15 +0500

    made notes

---
+++ flutterCrashCourseFiles/notes.dart
@@ -0,0 +1,38 @@
+Firebase file which is generated automatically needed to be 
+replaced everytime we pull from git.! 
+column=>mainaxis(y-axis),crossaxis(x-axis)
+
+row=>mainaxis
+ all flex properties are accessible 
+in column,row
+
+wrap=>mainaxis(basically flex)
+
+alignment:wrapalignment=>like mainAlignment in column & row
+
+runspace=>space bw two rows(called run elements)
+runAlignment=>if we want to align the run elements(we should
+only use alignment or run alignment not both).
+
+Card => makes a card of some color
+
+Sized-box=> we can make a box with some specified size
+
+Safe area=> to avoid on top cluttering
+
+Scaffold=>Full page 
+
+Scrolling:
+SingleChildScrollView=>maincomponent for multiple column
+ListView=>scrolling use we can use for single column
+Expanded=>works in column not in scrollview(overflowing handling
+means our children in the expanded can change their height and 
+width dynamically)
+
+Controllers:
+Basically the brain of the widget it is a class!
+we can make this to make different tasks in a 
+stateless r stateful widget
+
+Coding patterns
+I GIVE UP FLUTTER FOR NOW!
